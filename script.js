7faa1a51481807f7beb5d1c7740c98146e4a3ead
rmnrth4 <rothr8@bfh.ch>
2023-11-06 14:47:58 +0100

    Adjusting the form service
    Signed-off-by: Roman Roth rothr8@bfh.ch

--- script.js
+++ script.js
@@ -20,3 +20,35 @@ function openmenu() {
 function closemenu() {
   sidemenu.style.right = "-200px";
 }
+
+// contact form
+// https://www.youtube.com/watch?v=sGQSz22U8VM
+// https://smtpjs.com/
+// Email API: https://app.elasticemail.com/api/settings
+
+const form = document.forms["submit-to-mail"];
+const msg = document.getElementById("msg");
+
+function sendEmail() {
+  Email.send({
+    SecureToken: "c774c0dc-7d20-41ab-bd7d-fad73add28ae",
+    To: "romanroth44@gmail.com",
+    From: "romanroth44@gmail.com",
+    Subject: "New Contact Form Submit - portfolio-page",
+    Body:
+      "Name: " +
+      document.getElementById("Name").value +
+      "<br> Email: " +
+      document.getElementById("Email").value +
+      "<br> Message:<br>" +
+      document.getElementById("Message").value,
+  })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+}
