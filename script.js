0af9e695e336ddc4d94a3ca774727b315ffb9260
rmnrth4 <rothr8@bfh.ch>
2023-11-08 21:55:09 +0100

    Signed-off-by: Roman <rothr8@bfh.ch>
    implementing some more animations eg typed.js

--- script.js
+++ script.js
@@ -52,3 +52,18 @@ function sendEmail() {
     })
     .catch((error) => console.error("Error!", error.message));
 }
+
+// scroll animations
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
